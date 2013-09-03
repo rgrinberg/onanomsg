cc107a97f2869c776ed27a5089cf8ba2a23bbdce
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-09-03 13:35:41 -0400

    reworked example to do something

--- lib_test/run_tests.ml
+++ lib_test/run_tests.ml
@@ -1,7 +1,15 @@
 
 let () =
-  let sock = Onanomsg.socket ~domain:Onanomsg.Af_sp ~sock_type:`Req in
-  let endpoint = Onanomsg.bind sock ~transport:"inproc://test" in
-  Onanomsg.close sock
+  let receiver = Onanomsg.socket ~domain:Onanomsg.Af_sp ~sock_type:`Rep in
+  let _ = Onanomsg.bind receiver ~address:"inproc://test" in
+  let sender = Onanomsg.socket ~domain:Onanomsg.Af_sp ~sock_type:`Req in
+  let _ = Onanomsg.connect sender ~address:"inproc://test" in
+  let packet = "testing" in
+  ignore (Onanomsg.send sender packet);
+  let buf = String.create (String.length packet) in
+  let received = Onanomsg.recv_str receiver ~str:buf in
+  Onanomsg.close receiver;
+  Onanomsg.close sender;
+  print_endline ("received: " ^ buf)
 
 
