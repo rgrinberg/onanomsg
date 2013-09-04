ac93a142ad0dd0f28c490077100fd8e583430278
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-09-04 18:39:24 -0400

    pubsub example

---
+++ lib_test/test_pubsub.ml
@@ -0,0 +1,18 @@
+
+let () = 
+  let address = "inproc://t2t" in
+  let sub = Onanomsg.socket ~domain:Onanomsg.Af_sp ~sock_type:`Sub in
+  let endpoint = Onanomsg.connect sub ~address in
+  print_endline "Connecting subscriber";
+  let packet = "foo bar baz" in
+  let pub = Onanomsg.socket ~domain:Onanomsg.Af_sp ~sock_type:`Pub in
+  let endpoint = Onanomsg.bind pub ~address in
+  let (`Read _) = Onanomsg.send pub packet in
+  print_endline "published message";
+  let recv_msg = Onanomsg.recv sub in
+  print_endline "receiving in subscribers";
+  Printf.printf "Received: %s\n" recv_msg;
+  print_endline "closing publisher";
+  Onanomsg.close pub;
+  print_endline "closing subscriber";
+  Onanomsg.close sub
