91dbe8fc231c52c21fe11a42295194f8bac4f228
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-09-04 19:44:47 -0400

    fixed pubsub example

--- lib_test/test_pubsub.ml
+++ lib_test/test_pubsub.ml
@@ -3,6 +3,7 @@ let () =
   let address = "inproc://t2t" in
   let sub = Onanomsg.socket ~domain:Onanomsg.Af_sp ~sock_type:`Sub in
   let endpoint = Onanomsg.connect sub ~address in
+  Onanomsg.subscribe sub ~topic:"";
   print_endline "Connecting subscriber";
   let packet = "foo bar baz" in
   let pub = Onanomsg.socket ~domain:Onanomsg.Af_sp ~sock_type:`Pub in
