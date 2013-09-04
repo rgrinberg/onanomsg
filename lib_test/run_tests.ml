c735214fb2e2c183f87ee75b952908dbe4888fc8
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-09-03 22:00:49 -0400

    tests for recv, negative for now

--- lib_test/run_tests.ml
+++ lib_test/run_tests.ml
@@ -1,4 +1,19 @@
 
+(*let () =*)
+  (*let receiver = Onanomsg.socket ~domain:Onanomsg.Af_sp ~sock_type:`Rep in*)
+  (*let _ = Onanomsg.bind receiver ~address:"inproc://test" in*)
+  (*let sender = Onanomsg.socket ~domain:Onanomsg.Af_sp ~sock_type:`Req in*)
+  (*let _ = Onanomsg.connect sender ~address:"inproc://test" in*)
+  (*let packet = "testing" in*)
+  (*Printf.printf "packet(%s) length: %d\n" packet (String.length packet);*)
+  (*let (`Read sent) = Onanomsg.send sender packet in*)
+  (*let buf = String.create (String.length packet) in*)
+  (*let (`Read received) = Onanomsg.recv_str receiver ~str:buf in*)
+  (*Onanomsg.close receiver;*)
+  (*Onanomsg.close sender;*)
+  (*print_endline ("received: " ^ buf);*)
+  (*Printf.printf "sent: %d, received: %d\n" sent received*)
+
 let () =
   let receiver = Onanomsg.socket ~domain:Onanomsg.Af_sp ~sock_type:`Rep in
   let _ = Onanomsg.bind receiver ~address:"inproc://test" in
@@ -7,11 +22,7 @@ let () =
   let packet = "testing" in
   Printf.printf "packet(%s) length: %d\n" packet (String.length packet);
   let (`Read sent) = Onanomsg.send sender packet in
-  let buf = String.create (String.length packet) in
-  let (`Read received) = Onanomsg.recv_str receiver ~str:buf in
+  let received = Onanomsg.recv receiver in
   Onanomsg.close receiver;
   Onanomsg.close sender;
-  print_endline ("received: " ^ buf);
-  Printf.printf "sent: %d, received: %d\n" sent received
-
-
+  print_endline ("received: " ^ received);
