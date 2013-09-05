d588547fc6ceb780a85f467eecd627958ab2d650
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-09-04 21:26:24 -0400

    error variant

--- lib/onanomsg.ml
+++ lib/onanomsg.ml
@@ -1,6 +1,37 @@
 open Nanomsg
 
-exception Error of int * string
+type error =
+  | E_NOT_SUP
+  | E_PROTO_NO_SUPPORT
+  | E_NO_BUFFS
+  | E_NET_DOWN
+  | E_ADDR_IN_USE
+  | E_ADDR_NOT_AVAIL
+  | E_CONN_REFUSED
+  | E_IN_PROGRESS
+  | E_NOT_SOCK
+  | E_AF_NO_SUPPORT
+  | E_PROTO
+  | E_AGAIN
+  | E_BAD_F
+  | E_INVAL
+  | E_MFILE
+  | E_FAULT
+  | E_ACCCESS
+  | E_NET_RESET
+  | E_NET_UNREACH
+  | E_HOST_UNREACH
+  | E_NOT_CONN
+  | E_MSG_SIZE
+  | E_TIMED_OUT
+  | E_CONN_ABORTED
+  | E_CONN_RESET
+  | E_NO_PROTO_OPT
+  | E_IS_CONN
+  | E_TERM
+  | E_FSM
+
+exception Error of error * string
 
 type domains =
   | Af_sp
