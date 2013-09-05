22af089faf9ef9253dc50261eed870294f23e0e1
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-09-04 20:41:01 -0400

    fixed endpoint example

--- lib_test/test_endpoint.ml
+++ lib_test/test_endpoint.ml
@@ -6,6 +6,8 @@ let () =
                  , Onanomsg.socket ~domain:Onanomsg.Af_sp ~sock_type:`Sub) in
   let (_, _) = (Onanomsg.connect sub1 ~address:addr1
                , Onanomsg.connect sub2 ~address:addr2) in
+  Onanomsg.subscribe sub1 ~topic:"";
+  Onanomsg.subscribe sub2 ~topic:"";
   print_endline "connected subscribers";
   let packet = "one two three" in
   let pub = Onanomsg.socket ~domain:Onanomsg.Af_sp ~sock_type:`Pub in
@@ -13,9 +15,10 @@ let () =
   ignore (Onanomsg.bind pub ~address:addr2);
   ignore (Onanomsg.send pub packet);
   print_endline "published message";
-  Onanomsg.close pub;
-  print_endline "closed publisher";
   let (x1, x2) = Onanomsg.(recv sub1, recv sub2) in
   print_endline "receiving in subscribers";
   Printf.printf "Received: %s -- Received %s\n" x1 x2;
+  print_endline "closed publisher";
+  Onanomsg.close pub;
+  print_endline "closing subscriber";
   Onanomsg.(close sub1; close sub2)
