90875be15f06ffb3ec37f347a84975e5432c9657
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-11-12 01:09:05 -0500

    create symlinks that ocamlbuild used to create

--- reqrep.sh
+++ reqrep.sh
@@ -1,3 +1,4 @@
-./reqrep.native node0 ipc:///tmp/reqrep.ipc & node0=$! && sleep 1
-./reqrep.native node1 ipc:///tmp/reqrep.ipc
+./reqrep.native node0 ipc:///tmp/reqrep.ipc & node0=$!
+./reqrep.native node1 ipc:///tmp/reqrep.ipc test_message & node1=$!
+sleep 2
 kill $node0
