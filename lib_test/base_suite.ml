23c2b12356876bc0f70346db595c1c231951c2aa
Rudi Grinberg <rudi.grinberg@gmail.com>
2015-11-16 21:24:50 -0500

    Refactor tests
    * Split test suite into base_suite and lwt_suite so that the base suite
    can run without lwt (to respect opam's build-test + depopt)
    
    * Add some pretty printing to failing tests

--- lib_test/base_suite.ml
+++ lib_test/base_suite.ml
@@ -1,8 +1,5 @@
-open Lwt.Infix
 open OUnit2
-
 open Nanomsg
-module NB = Nanomsg_lwt
 
 let bind_addr_test ctx =
   let open Addr in
@@ -91,6 +88,12 @@ let connect_to_string_test ctx =
     (connect_of_string "tcp://dead::beef:1234")
     (`Tcp ((`V6 (Ipaddr.V6.of_string_exn "dead::beef"), None), 1234))
 
+let (>>=?) m msg f =
+  match m with
+  | `Ok v  -> f v
+  | `Error (s1, s2) ->
+    assert_failure (Printf.sprintf "%s: '%s' '%s'" msg s1 s2)
+
 let socket_test ctx =
   let domains = [AF_SP; AF_SP_RAW] in
   let protos = [Pair; Pub; Sub; Req; Rep; Push; Pull; Surveyor; Respondant; Bus] in
@@ -121,8 +124,7 @@ let socket_test ctx =
               ~err:(fun (e, m) -> failwith m);
          )
          protos
-    )
-    domains
+    ) domains
 
 let device_test ctx =
   (* int s1 = nn_socket (AF_SP_RAW, NN_REQ); *)
@@ -144,28 +146,6 @@ let send_recv_fd_test ctx =
   ignore @@ recv_fd sock;
   ignore @@ send_fd sock;
   close_exn sock
-
-let pair_test ctx =
-  let msgs = ["auie"; "uie,"; "yx.k"] in
-  let addr = `Inproc "rdv_point" in
-  let peer1 = socket_exn Pair in
-  let peer2 = socket_exn Pair in
-  let _ = bind peer1 addr in
-  let _ = connect peer2 addr in
-  let rec inner () =
-    Lwt_list.iter_s (fun msg ->
-        NB.send_string peer1 msg >>= fun () ->
-        NB.recv_string peer2 >>= fun recv_msg ->
-        assert_equal msg recv_msg; Lwt.return_unit
-      ) msgs >>= fun () ->
-    Lwt_list.iter_s (fun msg ->
-        NB.send_string peer2 msg >>= fun () ->
-        NB.recv_string peer1 >>= fun recv_msg ->
-        assert_equal msg recv_msg; Lwt.return_unit
-      ) msgs >|= fun () ->
-    close_exn peer1;
-    close_exn peer2
-  in Lwt_main.run @@ inner ()
 
 let reqrep_test ctx =
   let open CCError in
@@ -218,66 +198,6 @@ let pubsub_local_2subs_test ctx =
   assert_equal packet x1;
   assert_equal packet x2
 
-let tcp_pubsub_test ctx =
-  let open Nanomsg_lwt in
-  let inner () =
-    let port = 56352 in
-    wrap_error @@ socket Pub >>= fun pub ->
-    wrap_error @@ socket Sub >>= fun sub ->
-    wrap_error @@ set_ipv4_only pub false >>= fun () ->
-    wrap_error @@ set_ipv4_only sub false >>= fun () ->
-    let _ = bind pub @@ `Tcp (`All, port) in
-    let _ = connect sub @@ `Tcp ((`V6 Ipaddr.V6.localhost, None), port) in
-    wrap_error @@ Nanomsg.subscribe sub "" >>= fun () ->
-    let msg = "bleh" in
-    let len = String.length msg in
-    let recv_msg = Bytes.create @@ String.length msg in
-    let recv_msg' = Bytes.create @@ String.length msg in
-    let open Lwt.Infix in
-    let th = Nanomsg_lwt.send_string pub msg in
-    Nanomsg_lwt.recv_string sub >>= fun str ->
-    assert_equal (Lwt.Return ()) (Lwt.state th);
-    assert_equal msg str;
-    let th = Nanomsg_lwt.send_string_buf pub msg 0 len in
-    Nanomsg_lwt.recv_bytes_buf sub recv_msg 0 >>= fun (_:int) ->
-    assert_equal (Lwt.Return ()) (Lwt.state th);
-    assert_equal msg (Bytes.unsafe_to_string recv_msg);
-    let th = Nanomsg_lwt.send_bytes pub recv_msg in
-    Nanomsg_lwt.recv_bytes_buf sub recv_msg' 0 >|= fun (_:int) ->
-    assert_equal (Lwt.Return ()) (Lwt.state th);
-    assert_equal recv_msg recv_msg';
-    close_exn pub;
-    close sub
-  in Lwt_main.run @@ inner ()
-
-let pipeline_local_test ctx =
-  let open Nanomsg_lwt in
-  let msgs = [|"foo"; "bar"; "baz"|] in
-  let receiver addr =
-    wrap_error @@ socket Pull >>= fun s ->
-    wrap_error @@ bind s addr >>= fun _ ->
-    let rec inner n =
-      if n > 2
-      then wrap_error @@ close s
-      else
-        Nanomsg_lwt.recv_string s >>= fun m ->
-        (assert_equal msgs.(n) m; inner (succ n))
-    in
-    inner 0
-  in
-  let sender addr =
-    wrap_error @@ socket Push >>= fun s ->
-    wrap_error @@ connect s addr >>= fun _ ->
-    Lwt_list.iter_s (Nanomsg_lwt.send_string s) @@ Array.to_list msgs >>= fun () ->
-    Lwt_unix.yield () >>= fun () -> wrap_error @@ close s
-  in
-  Lwt_main.run @@
-    Lwt.join
-      [
-        sender (`Inproc "rdvpoint");
-        receiver (`Inproc "rdvpoint")
-      ]
-
 let suite =
   "Nanomsg">:::
   [
@@ -286,12 +206,9 @@ let suite =
     "connect_to_string" >:: connect_to_string_test;
     "socket" >:: socket_test;
     "send_recv_fd" >:: send_recv_fd_test;
-    "pair" >:: pair_test;
     "reqrep" >:: (fun a -> CCError.get_exn @@ reqrep_test a);
     "pubsub_local" >:: (fun a -> CCError.get_exn @@ pubsub_local_test a);
     "pubsub_local_2subs" >:: (fun a -> CCError.get_exn @@ pubsub_local_2subs_test a);
-    "tcp_pubsub" >:: (fun a -> CCError.get_exn @@ tcp_pubsub_test a);
-    "pipeline_local" >:: pipeline_local_test;
   ]
 
 let () =
